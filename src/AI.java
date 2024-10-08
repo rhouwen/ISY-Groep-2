6eaa63e76941cf75377bcadb6ebc86a975c1b5db
rickhouwen <r.houwen@st.hanze.nl>
2024-10-08 13:41:33 +0200

    Player, Mens, AI basis

--- src/AI.java
+++ src/AI.java
@@ -1,2 +1,22 @@
-public class AI {
+import java.util.Random;
+
+public class AI extends Player {
+    private Random random = new Random();
+
+    public AI(String name, Board board) {
+        super(name, board);
+    }
+
+    // AI move without intelligence just random
+    @Override
+    public void makeMove(Board opponentBoard) {
+        // random coordinates
+        int x = random.nextInt(8); //
+        int y = random.nextInt(8);
+
+        System.out.println(getName() + " schiet op (" + x + ", " + y + ")");
+
+        // Take shot
+        opponentBoard.takeShot(x, y);
+    }
 }
--- src/Mens.java
+++ src/Mens.java
@@ -1,2 +1,22 @@
-public class Mens {
+import java.util.Scanner;
+
+public class Mens extends Player {
+
+    public Mens(String name, Board board) {
+        super(name, board);
+    }
+
+    // Human shot
+    @Override
+    public void makeMove(Board opponentBoard) {
+        // Console input, later we need to make this working with GUI
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getName() + ", voer de coördinaten in om te schieten (bijv. 2 3): ");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        // make shot
+        opponentBoard.takeShot(x, y);
+    }
 }
+
