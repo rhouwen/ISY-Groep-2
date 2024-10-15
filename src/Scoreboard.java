5426079fd58937648da67b727319631ea869624f
rickhouwen <r.houwen@st.hanze.nl>
2024-10-15 12:48:29 +0200

    Werkend zonder finish

---
+++ src/Scoreboard.java
@@ -0,0 +1,30 @@
+public class Scoreboard {
+    private int playerScore;
+    private int aiScore;
+
+    public Scoreboard() {
+        playerScore = 0;
+        aiScore = 0;
+    }
+
+    public void incrementPlayerScore() {
+        playerScore++;
+    }
+
+    public void incrementAIScore() {
+        aiScore++;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
+    }
+
+    public int getAIScore() {
+        return aiScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Score - Speler: " + playerScore + " | AI: " + aiScore;
+    }
+}
--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,21 @@
-public class Ship{
-    
+public class Ship {
+    private final int length;
+    private int hitCount;  // Aantal hits dat het schip heeft opgelopen
+
+    public Ship(int length) {
+        this.length = length;
+        this.hitCount = 0;  // Begin met 0 hits
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void hit() {
+        hitCount++;  // Verhoog het hit-aantal
+    }
+
+    public boolean isSunk() {
+        return hitCount >= length;  // Controleer of het schip gezonken is
+    }
 }
