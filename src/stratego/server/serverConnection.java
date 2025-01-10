e344b1a65d9eca586bfe6647616b812a13885cb3
rhouwen <r.houwen@st.hanze.nl>
2025-01-10 14:01:57 +0100

    Server reacties verwerkt 0.1

--- src/stratego/server/serverConnection.java
+++ src/stratego/server/serverConnection.java
@@ -2,6 +2,7 @@ package stratego.server;
 
 import java.io.*;
 import java.net.Socket;
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class serverConnection implements Runnable {
@@ -10,6 +11,7 @@ public class serverConnection implements Runnable {
     private String ip_address;
     private String port;
     private String username;
+    private boolean placed;
 
 
     // Deze variabelen zijn nodig voor de server connectie
@@ -31,18 +33,45 @@ public class serverConnection implements Runnable {
         try {
             // TEST LINE DEZE KAN VERWIJDERD WORDEN LATER, DE ONDERSTE VERSIE WEL BEHOUDEN!!!!!! (33)
             System.out.println("Connecting to " + ip_address + ":" + port + " as " + username + ".....");
-
             
             client = new Socket(ip_address, Integer.parseInt(port));
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-
             // Login met onze username
             out.println("login " + username);
 
-            System.out.println("Connecting to " + ip_address + ":" + port + " as " + username + ".....");
+            System.out.println("Connected to " + ip_address + ":" + port + " as " + username + ".....");
 
+            String inputMessage;
+            while ((inputMessage = in.readLine()) != null) {
+                // Er is een game match gevonden tegen tegenstander x
+                if (inputMessage.startsWith("SVR GAME MATCH")) {
+                    System.out.println("Match gevonden");
+                    // Reset game state klasse
+                }
+
+                // Wij zijn aan de beurt om stukken te plaatsen of om te moven
+                if (inputMessage.startsWith("SVR GAME YOURTURN")) {
+                    placed = false;
+                    if (!placed) {
+                        // onze beurt om stukken te plaatsen
+                        // AI plaatst stukken
+                        // place <index (int)> <rank (string)>
+                        placed = true;
+                    }
+                    if (placed) {
+                        // onze beurt voor een zet
+                        // AI doet zet
+                        // move from <index (int)> to <index (int)>
+                    }
+
+                }
+
+                
+
+
+            }
 
             // HIER KOMEN DE INPUT MESSAGES --> DIE DAN DE NODIGE FUNCTIE VOOR DE AI AANROEPT.
 
